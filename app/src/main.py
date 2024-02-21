3492145230c601ab5f347d7a0ba87bada3eecd6c
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-02-21 10:22:41 +0100

    <feat>[main.py]:<adding the line get_input function into the main>

--- app/src/main.py
+++ app/src/main.py
@@ -1,7 +1,11 @@
 from Application import Application
+import threading
+import sys
 
 if __name__ == "__main__":
     app = Application()
-    app.register()
+    
+    app.receive_input()
     app.screen.mainloop()
+    app.quit()
 
