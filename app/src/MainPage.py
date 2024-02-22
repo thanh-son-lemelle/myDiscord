68a2ea4f4208c6930134ef25c66427430493d30d
lyes-hamici <lyes.hamici@laplateforme.io>
2024-02-22 16:09:28 +0100

    <feat>[create_widget]:<Add a scrollbar to the frame2>

--- app/src/MainPage.py
+++ app/src/MainPage.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 from customtkinter import *
 from customtkinter.assets import *
+from CTkListbox import *
 
 class MainPage(CTkFrame):
     def __init__(self, master):
@@ -14,14 +15,18 @@ class MainPage(CTkFrame):
         frame = CTkFrame(master=self, fg_color="#01b366", border_color="#FFFFFF", border_width=2, width=700)
         frame.pack(expand=False, side=ctk.LEFT, fill=ctk.Y)
 
-        frame2 = CTkFrame(master=self, fg_color="#383838", border_color="#FFFFFF", border_width=2, width=700)
-        frame2.pack(expand=True, fill='both')  
+        frame2 = CTkScrollableFrame(master=self, fg_color="#383838", border_color="#FFFFFF", border_width=2,orientation="vertical", scrollbar_button_color="#383838")
+        frame2.pack(expand=True, fill="both")
 
-        self.entry = CTkEntry(frame2, text_color="#000000", fg_color="#FFFFFF", width=1200)
-        self.entry.pack(side=ctk.BOTTOM, pady=10)  
+        
 
-        self.result_label = CTkLabel(frame2, text=self.master.read_message())
-        self.result_label.pack(side=ctk.TOP, pady=10)
+        self.entry = CTkEntry(self, text_color="#000000", fg_color="#FFFFFF", width=800)
+        self.entry.pack(side=ctk.TOP, pady=10)  
+
+        self.result_label = CTkLabel(master=frame2, text="",justify="left",font=("Arial", 16))
+        self.result_label.pack(anchor="w", expand=True,pady=10, padx=30)
+
+        self.test()
 
         label = CTkLabel(master=frame, text="Canaux", text_color="#000000")
         label.pack(side="top", pady=(10, 0))
@@ -32,8 +37,8 @@ class MainPage(CTkFrame):
         button_vocal = CTkButton(master=frame, text="Salon vocal", text_color="#000000", fg_color="#FFFFFF", hover_color="#FFDE00")
         button_vocal.pack(padx=30, pady=20)
 
-        button = CTkButton(frame2, text="Envoyer", command=self.on_clik_buttonSend, text_color="#000000", fg_color="#FFFFFF", hover_color="#01b366")
-        button.pack(side=ctk.BOTTOM, ipadx=40)
+        button = CTkButton(self, text="Envoyer", command=self.on_clik_buttonSend, text_color="#000000", fg_color="#FFFFFF", hover_color="#01b366")
+        button.pack(side=ctk.TOP,ipadx=40)
         
 
 
@@ -75,6 +80,14 @@ class MainPage(CTkFrame):
         message = self.entry.get()
         self.master.get_sending_message(message)
         self.entry.delete(0, 'end')
+
+
+    def test(self):
+        texte = ""
+        for tup in  self.master.read_message():
+            texte += str(tup) + "\n" + "\n"
+
+        self.result_label.configure(text=texte)
 
 
 
