033cde0a375a6c3bde5a5c2cafdd433d696c1682
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-02-28 18:12:01 +0100

    <refactor>[server class][methods]:<refactor of the methods that interact with the database due to a change in the data base. the type valuewas added and a correction was made into delete where there was a typo>

--- app/src/Server.py
+++ app/src/Server.py
@@ -4,21 +4,21 @@ class Server():
     def __init__(self) -> None:
         self.db = Db()
         
-    def create(self, server_name, description, owner, server_image, creation_date):
-        query = "INSERT INTO server (server_name, description, owner, server_image, creation_date) VALUES (%s, %s, %s, %s, %s)"
-        params = (server_name, description, owner, server_image, creation_date)
+    def create(self, server_name, description, type, owner, server_image):
+        query = "INSERT INTO server (server_name, description, type, owner, server_image) VALUES (%s, %s, %s, %s, %s)"
+        params = (server_name, description, type, owner, server_image)
         self.db.executeQuery(query, params)
 
     def read(self):
         query = 'SELECT * FROM server'
         return self.db.executeQuery(query)
     
-    def update(self, id, server_name, description, owner, server_image, creation_date):
-        query = 'UPDATE server SET server_name=%s, description=%s, owner=%s, server_image=%s, creation_date=%s WHERE id=%s'
-        params = (server_name, description, owner, server_image, creation_date, id)
+    def update(self, server_name, description, type, owner, server_image, creation_date, serverID):
+        query = 'UPDATE server SET server_name=%s, description=%s, type=%s, owner=%s, server_image=%s, creation_date=%s WHERE serverID=%s'
+        params = (server_name, description, type, owner, server_image, creation_date, serverID)
         self.db.executeQuery(query, params)
 
     def delete(self, id):
-        query = 'DELETE FROM server WHERE id=%s'
+        query = 'DELETE FROM server WHERE serverID=%s'
         params = (id)
         self.db.executeQuery(query, params)
