01ae5c07c9c3cd9ccd31ee92d57fee5db0018b56
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-03-06 11:38:41 +0100

    <feat>[get_channel_by_serverID]<refact>[on_click]:<adding get channel methods to be used in the init and deleting unused fucntion on on_click>

--- app/src/ServerButton.py
+++ app/src/ServerButton.py
@@ -1,30 +1,26 @@
-import customtkinter as ctk
+from customtkinter import CTkButton
 
-class ServerButton(ctk.CTkButton):
-    def __init__(self, master, server_name, server_icon, server_description, is_last=False):
+class ServerButton(CTkButton):
+    def __init__(self, master, main_page, server_name, serverID, userID):
         super().__init__(master)
+        self.main_page = main_page
         self.server_name = server_name
-        """self.server_id = server_id"""
-        self.server_icon = server_icon
-        self.server_description = server_description
-        self.is_last = is_last
-        """self.userID = userId"""
+        self.userID = userID
+        self.serverID = serverID
+        self.list_channels = self.get_channel_by_serverID(serverID)
+        print("list_channels into serverbutton", self.list_channels)
         self.create_widgets()
-        """self.userID = self.master.get_user_information()[0]"""
 
     def create_widgets(self):
         self.configure(text=self.server_name, command=self.on_click, fg_color="#000000", bg_color="#01b366", hover_color="#FFDE00", width=20, height=2, font=("Arial", 16))
 
     def on_click(self):
-        if self.is_last==False:
-            self.master.display_server_page(self.server_name, self.server_icon, self.server_description)
-        else:
-            self.add_server()
-
-    def add_server(self):
-        self.master.display_add_server_page()
-
-    def get_user_servers(self):
-        return self.master.get_user_servers(self.userID)[3]
+        print(f"Server {self.server_name, self.serverID} clicked")
+        self.main_page.swap_server_page(self.userID, self.serverID, self.list_channels)
+        print(f"Server {self.server_name} clicked")
     
-
+    def get_channel_by_serverID(self, serverID):
+        result = self.main_page.get_channel_by_serverID(serverID)
+        result.pop(0)
+        return result
+    
