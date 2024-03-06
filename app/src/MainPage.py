cbd3ced52fc14b296ba50017637ba735aa89d9ef
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-03-06 11:25:50 +0100

    <refactor>[create_widgets]<feat>[swap_server_page][get_channel_by_server_id][on_click_main_chat]:<rename creat_widgets into create_widgets, adding function to handle the behavior for the mainpage button>

--- app/src/MainPage.py
+++ app/src/MainPage.py
@@ -7,11 +7,12 @@ from .ServerButton import ServerButton
 
 import pygame
 
+
 class MainPage(CTkFrame):
     def __init__(self, master):
         super().__init__(master)
         self.running = True
-        self.creat_widgets()
+        self.create_widgets()
         self.commande = "" 
         self.recording = False
         self.recordings = []
@@ -20,15 +21,26 @@ class MainPage(CTkFrame):
                 self.recordings.append(elem)
         self.messages = []
         
-    def creat_widgets(self):
-
+    def create_widgets(self):
         server_frame = CTkFrame(master=self, fg_color="#01b366", border_color="#FFFFFF", border_width=2, width=80)
         server_frame.pack(expand=False, side=ctk.LEFT, fill=ctk.Y)
         
-        
-        print("test id",self.master.get_user_information()[0])
+        print("test id", self.master.get_user_information()[0])
         list_servers = self.master.get_user_server_by_userID(self.master.get_user_information()[0])
-        print("test list",list_servers)
+        print("test list", list_servers)
+
+        chat_textuel = CTkButton(master=server_frame, text="Main chat", text_color="#000000", fg_color="#FFFFFF", hover_color="#FFDE00", command=self.on_click_main_chat)
+        chat_textuel.pack(padx=0, pady=20)
+        
         for server in list_servers:
-            self.server_button = ServerButton(server_frame, server_name=server[4], server_icon=None, server_description=server[5], is_last=False)
-            self.server_button.pack()
+            server_button = ServerButton(server_frame, self, server_name=server[4], serverID=server[3], userID=self.master.get_user_information()[0])
+            server_button.pack()
+
+    def swap_server_page(self, server_name, serverID, list_channels):
+        self.master.swap_server_page(server_name, serverID, list_channels)
+
+    def get_channel_by_serverID(self, serverID):
+        return self.master.get_channel_by_serverID(serverID)
+    
+    def on_click_main_chat(self):
+        self.master.swap_server_page(self.master.get_user_information()[0], None, None)
